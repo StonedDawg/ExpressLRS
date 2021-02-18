8af8da1a2932f75bab01cd715fdabaea82ede885
StonedDawg <wazzlup@hotmail.com>
2021-02-18 16:45:15 +0700

    fix another weird conflict

--- src/src/config.h
+++ src/src/config.h
@@ -45,41 +45,6 @@ private:
     bool        m_modified;
 };
 
-///////////////////////////////////////////////////
-
-typedef struct {
-    uint32_t    version;
-    bool        isBound;
-    uint8_t     uid[UID_LEN];
-    uint8_t     powerOnCounter;
-} rx_config_t;
-
-class RxConfig
-{
-public:
-    void Load();
-    void Commit();
-
-    // Getters
-    bool     GetIsBound();
-    uint8_t* GetUID();
-    uint8_t  GetPowerOnCounter();
-    bool     IsModified();
-
-    // Setters
-    void SetIsBound(bool isBound);
-    void SetUID(uint8_t* uid);
-    void SetPowerOnCounter(uint8_t powerOnCounter);
-    void SetDefaults();
-    void SetStorageProvider(ELRS_EEPROM *eeprom);
-
-private:
-    tx_config_t m_config;
-    ELRS_EEPROM *m_eeprom;
-    bool        m_modified;
-};
-
-///////////////////////////////////////////////////
 
 typedef struct {
     uint32_t    version;
